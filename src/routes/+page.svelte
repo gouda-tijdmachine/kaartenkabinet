c84b3c99d11969da45e1ffcb61998d9a8035c47d
Mountaser164 <9015678@student.zadkine.nl>
2026-05-20 13:35:45 +0200

    welkomstpagina"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Rotterdam Tijdmachine</h1>
+<p>Welkom bij de Rotterdam Tijdmachine!</p>
