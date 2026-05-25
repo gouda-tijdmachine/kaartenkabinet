f5e90e29a3ece6c0df7a3c7576dd50f27de2dcbf
Mountaser164 <9015678@student.zadkine.nl>
2026-05-25 11:19:31 +0200

    Display map with MapLibre

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Rotterdam Tijdmachine</h1>
-<p>Welkom bij de Rotterdam Tijdmachine!</p>
+<script lang="ts">
+import { onMount, onDestroy } from 'svelte';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+let mapEl: HTMLDivElement;
+let map: any;
+
+const mapView = { center: [4.47917, 51.92528], zoom: 11 };
+
+onMount(async () => {
+  const maplibregl = (await import('maplibre-gl')).default;
+  map = new maplibregl.Map({
+    style: 'https://tiles.openfreemap.org/styles/liberty', // jouw style URL
+    container: mapEl,                                       // HTML element target
+    maxPitch: 0,                                            // geen 3D tilt
+    center: mapView.center,
+    zoom: mapView.zoom
+  });
+
+  // optionele navigatie knoppen
+  map.addControl(new maplibregl.NavigationControl(), 'top-right');
+
+  map.on('load', () => map.resize());
+});
+
+onDestroy(() => {
+  map?.remove();
+});
+</script>
+
+<style>
+#map { width: 100%; height: 80vh; }
+header { padding: 0.5rem; background: #fff; }
+</style>
+
+<header>
+  <h1>Rotterdam Tijdmachine</h1>
+</header>
+<div id="map" bind:this={mapEl}></div>
